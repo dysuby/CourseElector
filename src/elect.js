159e40f093ebd77b48355354f52c7abb1eca7117
Veg_chic <h_foryou@163.com>
2018-07-13 21:56:07 +0800

    增加随机性和固定频率

--- src/elect.js
+++ src/elect.js
@@ -3,13 +3,10 @@ const qs = require('querystring');
 const cheerio = require('cheerio');
 const cas = require('./cas');
 const { headers, casUrl, electUrl } = require('../config/reqConfig');
-const { target, delay } = require('../config/user');
+const { target } = require('../config/user');
 const msg = require('../config/msg');
 
 async function begin() {
-  function parse(href) {
-    return qs.parse(url.parse(href).query);
-  }
   const rp = await cas();
   const opt = {
     uri: casUrl,
@@ -21,52 +18,58 @@ async function begin() {
   const $ = await rp(opt);
 
   // 获得参数
+  function parse(href) {
+    return qs.parse(url.parse(href).query);
+  }
+
   const sid = $('#sid').val();
   const pro_zid = parse($('a')[2].attribs.href).xkjdszid;
   const public_zid = parse($('a')[4].attribs.href).xkjdszid;
   const len = target.length;
 
   // 开始选课
+  let counter = 0;
+
   target.forEach(v => {
+    setTimeout(() => {
+      elect(v);
+    }, 2000 + Math.random() * 1000);
+  });
+
+  async function elect(v) {
     const form = {
       jxbh: v.course,
       xkjdszid: v.type === '公选' ? public_zid : pro_zid,
       sid
     };
-    let counter = 0;
-    const id = setInterval(() => {
-      rp.post({
-        url: electUrl,
-        headers,
-        form,
-        transform: function(body) {
-          return cheerio.load(body);
+    rp.post({
+      url: electUrl,
+      headers,
+      form,
+      transform: function(body) {
+        return cheerio.load(body);
+      }
+    })
+      .then($ => {
+        const result = JSON.parse($('textarea').text());
+        if (result.err.code === 0) {
+          console.log(`${v.course} 选课成功`);
+          --len;
+          if (!len) process.exit(0);
+        } else {
+          if (counter === Number.MAX_SAFE_INTEGER) counter = 0;
+          else ++counter;
+          console.log(
+            `${form.jxbh} 第${counter}次选课失败: ${msg[result.err.code]}`
+          );
+          setTimeout(elect, 2000 + Math.random() * 1000);
         }
       })
-        .then($ => {
-          const result = JSON.parse($('textarea').text());
-          if (result.err.code === 0) {
-            console.log('选课成功');
-            clearInterval(id);
-            --len;
-            if (!len) process.exit(0);
-          } else {
-            if (counter === Number.MAX_SAFE_INTEGER) counter = 0;
-            else ++counter;
-            console.log(
-              `${form.jxbh} 第${counter}次选课失败: ${
-                msg[result.err.code]
-              }，continue...`
-            );
-          }
-        })
-        .catch(err => {
-          clearInterval(id);
-          console.log('出错啦');
-          process.exit(0);
-        });
-    }, delay);
-  });
+      .catch(err => {
+        console.log('出错啦');
+        process.exit(0);
+      });
+  }
 }
 
 module.exports = begin;
